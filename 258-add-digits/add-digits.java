46819f1b8897ddc2b349e2fd1189ae9c2151065b
Mr_U <abhaygautam029@gmail.com>
2026-02-04 22:44:16 +0530

    Time: 1 ms (97.72%) | Memory: 42.9 MB (7.43%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int addDigits(int num) {
         return recur(num);
@@ -18,3 +19,13 @@ class Solution {
         
     }
 }
+*/
+class Solution {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if(num%9==0) return 9;
+
+        return num % 9;
+    }
+}
+
